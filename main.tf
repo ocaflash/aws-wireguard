64e07d8030e032f83efdb76404fd60381a7d3171
ocaflash <oCa.flash@gmail.com>
2022-08-19 16:48:54 +0300

    q

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
-module "networking" {
-  source  = "cn-terraform/networking/aws"
-  version = "2.0.13"
-
-  name_prefix = substr(format("%s-%s", var.name_prefix, var.environment), 0, 32)
-
-  vpc_cidr_block                              = var.vpc_cidr_block
-  availability_zones                          = var.availability_zones
-  public_subnets_cidrs_per_availability_zone  = var.public_subnets_cidrs
-  private_subnets_cidrs_per_availability_zone = var.private_subnets_cidrs
-  single_nat                                  = true
-}
+# module "networking" {
+#   source  = "cn-terraform/networking/aws"
+#   version = "2.0.13"
+#
+#   name_prefix = substr(format("%s-%s", var.name_prefix, var.environment), 0, 32)
+#
+#   vpc_cidr_block                              = var.vpc_cidr_block
+#   availability_zones                          = var.availability_zones
+#   public_subnets_cidrs_per_availability_zone  = var.public_subnets_cidrs
+#   private_subnets_cidrs_per_availability_zone = var.private_subnets_cidrs
+#   single_nat                                  = true
+# }
 
 resource "aws_security_group" "wireguard" {
   name        = "${var.name_prefix}-${var.environment}-vpn"
@@ -65,10 +65,10 @@ data "template_file" "wireguard_userdata" {
 }
 
 resource "aws_instance" "wireguard" {
-  ami                    = "ami-0fb653ca2d3203ac1"
-  instance_type          = "t3a.micro" #"t3a.nano"
-  key_name               = aws_key_pair.ssh_key.key_name
-  subnet_id              = module.networking.public_subnets_ids[0]
+  ami           = "ami-0fb653ca2d3203ac1"
+  instance_type = "t3a.micro" #"t3a.nano"
+  key_name      = aws_key_pair.ssh_key.key_name
+  # subnet_id              = module.networking.public_subnets_ids[0]
   vpc_security_group_ids = [aws_security_group.wireguard.id]
   user_data              = data.template_file.wireguard_userdata.rendered
 
