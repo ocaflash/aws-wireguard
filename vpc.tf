488fff8b9a8a4d13f2934fe63b16724b2e3d1e28
ocaflash <oCa.flash@gmail.com>
2022-08-20 14:38:04 +0300

    3

--- vpc.tf
+++ vpc.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "wireguard-vpc" {
 resource "aws_subnet" "subnet-public" {
   vpc_id            = aws_vpc.wireguard-vpc.id
   cidr_block        = var.public_subnets_cidrs[0]
-  availability_zone = var.availability_zones
+  availability_zone = var.availability_zones[0]
   tags = {
     Name = "Public network"
   }
@@ -18,7 +18,7 @@ resource "aws_subnet" "subnet-public" {
 resource "aws_subnet" "subnet-private" {
   vpc_id            = aws_vpc.wireguard-vpc.id
   cidr_block        = var.private_subnets_cidrs[0]
-  availability_zone = var.availability_zones
+  availability_zone = var.availability_zones[0]
   tags = {
     Name = "Private network"
   }
