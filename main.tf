c1948a865666c58e36743eb82aeda3164afee6b9
ocaflash <oCa.flash@gmail.com>
2022-08-20 14:30:07 +0300

    3

--- main.tf
+++ main.tf
@@ -1,64 +1,16 @@
-module "networking" {
-  source  = "cn-terraform/networking/aws"
-  version = "2.0.13"
+# module "networking" {
+#   source  = "cn-terraform/networking/aws"
+#   version = "2.0.13"
+#
+#   name_prefix = substr(format("%s-%s", var.name_prefix, var.environment), 0, 32)
+#
+#   vpc_cidr_block                              = var.vpc_cidr_block
+#   availability_zones                          = var.availability_zones
+#   public_subnets_cidrs_per_availability_zone  = var.public_subnets_cidrs
+#   private_subnets_cidrs_per_availability_zone = var.private_subnets_cidrs
+#   single_nat                                  = true
+# }
 
-  name_prefix = substr(format("%s-%s", var.name_prefix, var.environment), 0, 32)
-
-  vpc_cidr_block                              = var.vpc_cidr_block
-  availability_zones                          = var.availability_zones
-  public_subnets_cidrs_per_availability_zone  = var.public_subnets_cidrs
-  private_subnets_cidrs_per_availability_zone = var.private_subnets_cidrs
-  single_nat                                  = true
-}
-
-resource "aws_security_group" "wireguard" {
-  name        = "${var.name_prefix}-${var.environment}-vpn"
-  description = "Communication to and from VPC endpoint"
-  vpc_id      = module.networking.vpc_id
-
-  tags = {
-    Name = "${var.name_prefix}-${var.environment}"
-  }
-
-  ingress {
-    protocol    = "tcp"
-    from_port   = 22
-    to_port     = 22
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    protocol    = "udp"
-    from_port   = 51820
-    to_port     = 51820
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    protocol    = "tcp"
-    from_port   = 8080
-    to_port     = 8080
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ##################
-  egress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_eip" "wireguard" {
-  instance = aws_instance.wireguard.id
-  vpc      = true
-}
-
-resource "aws_eip_association" "wireguard" {
-  instance_id   = aws_instance.wireguard.id
-  allocation_id = aws_eip.wireguard.id
-}
 
 data "template_file" "wireguard_userdata" {
   template = file("resources/cloud-init.yaml")
@@ -68,6 +20,7 @@ resource "aws_instance" "wireguard" {
   ami           = data.aws_ami.latest_ubuntu.id
   instance_type = "t3a.micro" #"t3a.nano"
   key_name      = aws_key_pair.ssh_key.key_name
+  subnet_id     = aws_subnet.subnet-public.public_subnets_ids[0]
   # subnet_id                   = module.networking.public_subnets_ids[0]
   vpc_security_group_ids      = [aws_security_group.wireguard.id]
   associate_public_ip_address = true
---
+++ vpc.tf
@@ -0,0 +1,82 @@
+
+resource "aws_vpc" "wireguard-vpc" {
+  cidr_block           = var.vpc_cidr_block
+  enable_dns_hostnames = true
+  tags {
+    Name = "${var.name_prefix}-${var.environment}-vpc"
+  }
+}
+
+## Public
+resource "aws_subnet" "subnet-public" {
+  vpc_id            = aws_vpc.wireguard_vpc.id
+  cidr_block        = var.public_subnets_cidrs
+  availability_zone = var.region + "a"
+  tags = {
+    Name = "Public network"
+  }
+}
+
+## Private
+resource "aws_subnet" "subnet-private" {
+  vpc_id            = aws_vpc.wireguard_vpc.id
+  cidr_block        = var.private_subnets_cidrs
+  availability_zone = var.aws_region + "b"
+  tags = {
+    Name = "Private network"
+  }
+}
+
+#Internet  GW
+resource "aws_internet_gateway" "wireguard" {
+  vpc_id = aws_vpc.wireguard_vpc.id
+}
+
+resource "aws_security_group" "wireguard" {
+  name        = "${var.name_prefix}-${var.environment}-vpn"
+  description = "Communication to and from VPC endpoint"
+  vpc_id      = aws_vpc.wireguard_vpc.id
+
+  tags = {
+    Name = "${var.name_prefix}-${var.environment}"
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "udp"
+    from_port   = 51820
+    to_port     = 51820
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 8080
+    to_port     = 8080
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ##################
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_eip" "wireguard" {
+  instance = aws_instance.wireguard.id
+  vpc      = true
+}
+
+resource "aws_eip_association" "wireguard" {
+  instance_id   = aws_instance.wireguard.id
+  allocation_id = aws_eip.wireguard.id
+}
