e97528c99dfa9c445a487572d325cb0b78186016
ocaflash <oCa.flash@gmail.com>
2022-08-20 14:36:30 +0300

    3

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "aws_instance" "wireguard" {
   ami           = data.aws_ami.latest_ubuntu.id
   instance_type = "t3a.micro" #"t3a.nano"
   key_name      = aws_key_pair.ssh_key.key_name
-  subnet_id     = aws_subnet.subnet-public.public_subnets_ids[0]
+  subnet_id     = aws_subnet.subnet-public.id
   # subnet_id                   = module.networking.public_subnets_ids[0]
   vpc_security_group_ids      = [aws_security_group.wireguard.id]
   associate_public_ip_address = true
