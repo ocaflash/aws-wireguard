99714e8a8551081563d1a04679ba59393c49af41
ocaflash <oCa.flash@gmail.com>
2022-08-22 11:52:32 +0300

    1

--- _variables.tf
+++ _variables.tf
@@ -8,35 +8,11 @@ variable "region" {
   default     = "us-west-2"
 }
 
-variable "availability_zones" {
-  description = "Region to be used for AWS resources"
-  default     = ["us-west-2a"]
-}
-
 variable "tags" {
   description = "A mapping of tags to assign to the resource"
   type        = map(string)
   default     = {}
 }
 
-variable "vpc_cidr_block" {
-  default = "192.168.0.0/16"
-}
-
-variable "public_subnets_cidrs" {
-  description = "CIDR for the Public Subnet"
-  type        = list(string)
-  default     = ["192.168.32.0/19"]
-}
-
-variable "private_subnets_cidrs" {
-  description = "CIDR for the Private Subnet"
-  type        = list(string)
-  default     = ["192.168.128.0/19"]
-}
-
-
-# variable "ssh_private_key" {}
-# variable "ssh_public_key" {}
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
--- main.tf
+++ main.tf
@@ -1,27 +1,10 @@
-# module "networking" {
-#   source  = "cn-terraform/networking/aws"
-#   version = "2.0.16"
-#
-#   name_prefix = substr(format("%s-%s", var.name_prefix, var.environment), 0, 32)
-#
-#   vpc_cidr_block                              = var.vpc_cidr_block
-#   availability_zones                          = var.availability_zones
-#   public_subnets_cidrs_per_availability_zone  = var.public_subnets_cidrs
-#   private_subnets_cidrs_per_availability_zone = var.private_subnets_cidrs
-#   single_nat                                  = true
-# }
-
-
-
 data "template_file" "wireguard_userdata" {
   template = file("resources/cloud-init.yaml")
 }
 
 resource "aws_instance" "wireguard" {
-  ami           = data.aws_ami.latest_ubuntu.id
-  instance_type = "t3a.micro" #"t3a.nano"
-  # key_name                    = aws_key_pair.ssh_key.key_name
-  # subnet_id                   = module.networking.public_subnets_ids[0]
+  ami                         = data.aws_ami.latest_ubuntu.id
+  instance_type               = "t3a.micro"
   vpc_security_group_ids      = [aws_security_group.wireguard.id]
   associate_public_ip_address = true
   user_data                   = data.template_file.wireguard_userdata.rendered
@@ -31,23 +14,7 @@ resource "aws_instance" "wireguard" {
   }
 }
 
-# resource "aws_eip" "wireguard" {
-#   instance = aws_instance.wireguard.id
-#   vpc      = true
-#   lifecycle {
-#     prevent_destroy = true
-#   }
-#   tags = {
-#     Name = "${var.name_prefix}-${var.environment}"
-#   }
-# }
-
 resource "aws_eip_association" "wireguard" {
   instance_id   = aws_instance.wireguard.id
   allocation_id = aws_eip.wireguard.id
 }
-#
-# resource "aws_key_pair" "ssh_key" {
-#   key_name   = format("%s-wg-server-ssh-key", var.environment)
-#   public_key = file(var.ssh_public_key)
-# }
